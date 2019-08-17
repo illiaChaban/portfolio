29e5aefbf938462276d3681588a2b942dbc56e4c
Illia Chaban <illia.chaban.8@gmail.com>
2019-08-17 18:42:12 -0400

    save projects. added shadow, using svg morph

--- docs/js/lazy.js
+++ docs/js/lazy.js
@@ -18,7 +18,7 @@ window.lazy = {};
     },
     projects: {
       dependancies: [
-        ['https://cdnjs.cloudflare.com/ajax/libs/animejs/2.2.0/anime.min.js', 'script'],
+        ['https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js', 'script'],
         ['js/projects/projects.js', 'script'],
         ['css/projects.css', 'css'],
       ],
--- docs/js/projects/projects.js
+++ docs/js/projects/projects.js
@@ -4,11 +4,11 @@ let initialized = false;
 window.initProjects = () => {
   if (initialized) return;
   // Refs
-  const project = document.querySelector('.project');
-        wrapCta  = project.querySelector('#wrap-cta'),
-        btnCta   = project.querySelector('#cta'),
-        content  = project.querySelector('.content'),
-        btnClose = project.querySelector('#close');
+  const project = document.querySelector('.project'),
+        containerClosed = project.querySelector('.container-closed'),
+        containerOpen = project.querySelector('.container-open');
+        // btnOpen   = project.querySelector('button.open'),
+        // btnClose = project.querySelector('button.close');
   
   
   // Anime.js Commons Values for SVG Morph
@@ -18,38 +18,68 @@ window.initProjects = () => {
     duration: 600,
     loop: false
   };
+
+
+
+
+
+  let initialPoints = project.querySelector('.polymorph').getAttribute('points');
+  console.log(initialPoints)
+
+
+  // console.log(openSvg)
+  // console.log(openSvg.pause());
+
+  let openSvg, closeSvg; //saving animations to be able to stop them
+
+  const showContent = () => {
+    // Elements apparence
+    project.classList.add('active');
+
+    if (closeSvg) closeSvg.pause();
+
+    // Morph SVG
+    openSvg = anime({
+      ...common,
+      points: [
+        // { value: '100,100 0,100 98,98 100,0' }
+        // { value: '98,98 0,100 100,100 100,0' }
+        // { value: '98,98 0,100 100,100 100,0' }
+        { value: '0,0 0,100 100,100 100,0' }
+      ],
+    });
+
+    // openSvg.restart();
+  };
+
+  const hideContent = () => {
+    // Elements apparence
+    project.classList.remove('active');
+    
+    if (openSvg) openSvg.pause();
+
+    // Morph SVG
+    closeSvg = anime({
+      ...common,
+      points: [
+        // { value: '100,100 0,100 0,0 100,0' }
+        // { value: '0,0 0,100 100,100 100,0' }
+        { value: initialPoints }
+      ]
+    }); 
+
+  }
   
   
   // Show content
-  btnCta.addEventListener('click', () => {
-    // Elements apparence
-    wrapCta.classList.remove('active');
-    content.classList.add('active');
-    
-    // Morph SVG
-    anime({
-      ...common,
-      points: [
-        { value: '100,100 0,100 99,99 100,0' }
-      ],
-    });
-  });
+  containerClosed.addEventListener('click', showContent);
+  containerOpen.addEventListener('click', hideContent);
+
+  // project.addEventListener('mouseenter', showContent);
+  // project.addEventListener('mouseleave', hideContent);
   
   
   // Hide content  
-  btnClose.addEventListener('click', () => {
-    // Elements apparence
-    content.classList.remove('active');
-    wrapCta.classList.add('active');
-    
-    // Morph SVG
-    anime({
-      ...common,
-      points: [
-        { value: '100,100 0,100 0,0 100,0' }
-      ]
-    }); 
-  });
 
   initialized = true;
 }
